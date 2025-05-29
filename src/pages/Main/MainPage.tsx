939e7245f4a809ba78169175afca663f5ecea940
Roman Ulitin <kurt-77@mail.ru>
2025-05-29 17:56:46 +0500

    добавил в компонент main нижнюю строку

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -14,8 +14,17 @@ export const MainPage = () => {
           <h1 className={styles.main_title}>SHIFT</h1>
           <p className={styles.main_text}>creavtive</p>
         </div>
-        <p className={styles.main_subtext}>Мы создаем сайты и бренды под ключ</p>
+        <p className={styles.main_subtext}>
+          Мы создаем сайты и бренды под ключ
+        </p>
+        <ul className={styles.main_list}>
+          <li className={styles.main_list_item}>Гарантия результата</li>
+          <li className={styles.main_list_item}>Индивидуальный подход</li>
+          <li className={styles.main_list_item}>Юридическая чистота</li>
+          <li className={styles.main_list_item}>Полный цикл</li>
+        </ul>
       </section>
+      <section className={styles.heading}></section>
     </Layout>
   );
 };
