3c3923ad5e7d2f0e6da1566ee8899d93b5a8f99e
Roman Ulitin <kurt-77@mail.ru>
2025-06-20 16:51:15 +0500

    рефакторинг кода, удаление дублирования, в секции пакеты услуг

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -13,6 +13,7 @@ import { ContactFormMainPage } from "../../components/ContactForm/ContactFormMai
 import RunningLine from "../../components/RunningLine/RunningLine";
 import { caseCardItems } from "../../components/CaseCard/caseCardItems";
 import { serviceCardItems } from "../../components/ServiceCard/serviceCardItems";
+import { tariffCardItems } from "../../components/TariffCard/tariffCardItems";
 
 export const MainPage = () => {
   const [isFormOpen, setIsFormOpen] = useState(false);
@@ -56,15 +57,7 @@ export const MainPage = () => {
         <h3 className={styles.services_title}>Что мы делаем</h3>
         <div className={styles.services_container}>
           {serviceCardItems.map((item, index) => (
-            <ServiceCard
-              key={index}
-              title={item.title}
-              description={item.description}
-              price={item.price}
-              duration={item.duration}
-              iconName={item.iconName}
-              includesList={item.includesList}
-            />
+            <ServiceCard key={index} {...item} />
           ))}
         </div>
       </section>
@@ -173,93 +166,9 @@ export const MainPage = () => {
         <h4 className={styles.price_subtitle}>тарифы</h4>
         <h3 className={styles.price_title}>Пакеты услуг</h3>
         <div className={styles.price_container}>
-          <TariffCard
-            active={true}
-            title="Старт"
-            subtitle="Подходит для быстрого запуска бизнеса"
-            iconName="start"
-            features={[
-              { text: "Дизайн лендинга (1 страница)", included: true },
-              { text: "Подключение формы заявки", included: true },
-              {
-                text: "Базовая адаптация под мобильные устройства",
-                included: true,
-              },
-              { text: "Подключение домена и хостинга", included: true },
-              {
-                text: "Первичная SEO-настройка (метатеги, фавикон)",
-                included: true,
-              },
-              {
-                text: "Срок — 7 дней",
-                included: true,
-              },
-              { text: "Другие разделы сайта", included: false },
-              { text: "Персональный менеджер", included: false },
-              { text: "Дополнительные языковые версии", included: false },
-            ]}
-          />
-          <TariffCard
-            title="Стандарт"
-            subtitle="Оптимально для малого и среднего бизнеса"
-            iconName="standart"
-            features={[
-              { text: "Многостраничный сайт (до 7 экранов)", included: true },
-              {
-                text: "Дизайн и прототипирование всех ключевых разделов",
-                included: true,
-              },
-              { text: "Адаптив под мобильные устройства", included: true },
-              {
-                text: "Подключение форм заявок и обратной связи",
-                included: true,
-              },
-              {
-                text: "Интеграция с CMS (WordPress или Tilda)",
-                included: true,
-              },
-              { text: "Базовое наполнение контентом", included: true },
-              {
-                text: "Первичная SEO-настройка (метатеги, фавикон, карта сайта)",
-                included: true,
-              },
-              { text: "14 дней технической поддержки", included: true },
-              { text: "Персональная фото/видеосъемка", included: false },
-              { text: "Мультиязычная версия", included: false },
-            ]}
-          />
-          <TariffCard
-            title="Премиум"
-            subtitle="Подходит для быстрого запуска бизнеса"
-            iconName="premium"
-            features={[
-              {
-                text: "Индивидуальный дизайн всех страниц (до 20 экранов)",
-                included: true,
-              },
-              {
-                text: "Глубокое прототипирование пользовательских сценариев",
-                included: true,
-              },
-              { text: "Персональный менеджер проекта", included: true },
-              {
-                text: "Эксклюзивная анимация и интерактивные элементы",
-                included: true,
-              },
-              {
-                text: "Полная адаптивность под все устройства",
-                included: true,
-              },
-              { text: "Мультиязычная версия (RU/EN)", included: true },
-              { text: "Интеграция с CRM и расширенные формы", included: true },
-              {
-                text: "Заполнение уникальным контентом (текст/графика)",
-                included: true,
-              },
-              { text: "Расширенная SEO-оптимизация", included: true },
-              { text: "30 дней технической поддержки", included: true },
-            ]}
-          />
+          {tariffCardItems.map((item, index) => (
+            <TariffCard key={index} {...item} />
+          ))}
         </div>
       </section>
 
