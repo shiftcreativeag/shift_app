f92401d87a31f4ddc60aa47c21f372f04f94ac96
Roman Ulitin <kurt-77@mail.ru>
2025-06-20 17:00:04 +0500

    рефакторинг кода, удаление дублирования, в секции команда

---
+++ src/components/MemberCard/memberCardItems.ts
@@ -0,0 +1,32 @@
+export const memberCardItems = [
+  {
+    name: "Александр Матвеев",
+    role: "Генеральный директор",
+    imageName: "/gif/member-animation.gif",
+  },
+  {
+    name: "Вячеслав Працюк",
+    role: "Руководитель проектов",
+    imageName: "/gif/member-animation.gif",
+  },
+  {
+    name: "Ирина Ковалёва",
+    role: "UX/UI-дизайнер",
+    imageName: "/gif/member-animation.gif",
+  },
+  {
+    name: "Роман Улитин",
+    role: "Фронтенд-разработчик",
+    imageName: "/gif/member-animation.gif",
+  },
+  {
+    name: "Денис Соловьёв",
+    role: "Бэкенд-разработчик",
+    imageName: "/gif/member-animation.gif",
+  },
+  {
+    name: "Игорь Васильев",
+    role: "Тестировщик / QA",
+    imageName: "/gif/member-animation.gif",
+  },
+];
